1ce61070409ad396d75deb7f2bf89e58c81f13b7
k8e <k8eSchaffer@gmail.com>
2019-10-27 14:09:36 -0700

    Finally committing these updates from forever ago

--- cursor-party.js
+++ cursor-party.js
@@ -13,7 +13,7 @@ var id,
     posY = 0;
 var hasCursor = false;
 var myCursor = document.createElement('div');
-    myCursor.className = "cursor";
+    myCursor.className = "cursor" + " " + "cursor-" + getType();
 
 var cursorOffset = {
     top: 10,
@@ -44,10 +44,12 @@ document.addEventListener("mousemove", function(event) {
     );
 
     // Send update to server
-    socket.emit("motion", {
-        x: posX + window.pageXOffset,
-        y: posY + window.pageYOffset 
-    });
+    if (id) {
+      socket.emit("motion", {
+          x: posX + window.pageXOffset,
+          y: posY + window.pageYOffset 
+      });
+    }
 
     event.preventDefault();
 }, false);
@@ -74,30 +76,19 @@ document.addEventListener("touchmove", function(event) {
     );
 
     // Send update to server
-    socket.emit("motion", {
-        x: posX + window.pageXOffset,
-        y: posY + window.pageYOffset 
-    });
+    if (id) {
+      socket.emit("motion", {
+          x: posX + window.pageXOffset,
+          y: posY + window.pageYOffset 
+      });
+    }
 
     event.preventDefault(); // prevent scrolling
 }, {passive: false});
 
 /* Listen for socket events */
 
-// Successful connection
-socket.on("connection_established", function(info) {
-    // Retrieve user ID from server
-    id = info.id;
-
-    // Retrieve existing pals
-    for (var userId in info.allPals) {
-        var pal = info.allPals[userId];
-        createNewCursor(userId);
-        moveCursor(pals[userId], pal.x, pal.y);
-    }
-});
-
-// New connection? Possibly server reconnection
+// Initial handshake (or possibly a re-handshake in case of server reboot)
 socket.on("handshake", function(callback) {
     // Pull together whatever data we can send to the server
     var clientInfo = {
@@ -108,10 +99,35 @@ socket.on("handshake", function(callback) {
     callback(clientInfo);
 });
 
+// Successful connection
+socket.on("connection_established", function(info) {
+    // Retrieve user ID from server
+    id = info.id;
+  
+    if (id) {
+      // Set online count
+      if (!info.allPals.length || info.allPals.length < 1)
+        setOnlineCount(1);
+
+      // Retrieve existing pals
+      for (var userId in info.allPals) {
+          var pal = info.allPals[userId];
+          if (pal.x && pal.y) { 
+              createNewCursor(userId); // Adds new cursor to "pals"
+              moveCursor(pals[userId], pal.x, pal.y);
+          }
+      }
+    }
+    else {
+      console.log("FYI: cursor-party has a limit on connections per IP address!");
+    }
+
+});
+
 // Client mouse movement
 socket.on("motion", function(event) {
     if (!(event.x && event.y) || !event.userId) return;
-
+  
     // Create a cursor for newcomer
     if (!pals[event.userId] && event.userId != id)
         createNewCursor(event.userId);
@@ -120,29 +136,28 @@ socket.on("motion", function(event) {
     moveCursor(pals[event.userId], event.x, event.y);
 });
 
-// Another client arrives
-socket.on("arrival", function(arriver) {
-    console.log("✧ Welcome new friend: " + arriver.id)
-})
-
 // A client disconnects
 socket.on("departure", function(event) {
     var userId = event.id;
     var delCursor = pals[userId];
 
     // Remove cursor
-    delCursor.parentElement.removeChild(delCursor);
-    delete pals[userId];
+    if (delCursor) {
+        delCursor.parentElement.removeChild(delCursor);
+        delete pals[userId];
+
+        setOnlineCount(Object.keys(pals).length + 1);
+    }
 });
 
 
 /* Assisting functions */
 
 function createNewCursor(userId) {
-    var newCursor = document.createElement('div');
-    newCursor.className = "cursor";
+    var newCursor = document.createElement('div');    
     pals[userId] = newCursor;
     container.appendChild(pals[userId]);
+    setOnlineCount(Object.keys(pals).length + 1);  
 }
 
 function moveCursor(icon, x, y) {
@@ -151,3 +166,8 @@ function moveCursor(icon, x, y) {
         icon.style.top = y + 'px';
     }
 };
+
+function setOnlineCount(num) {
+    var counter = document.getElementById("online-count");
+    counter.innerHTML = num;
+}
--- server.js
+++ server.js
@@ -2,24 +2,25 @@
  * Server
 **/
 
-// Environment variables
-require('dotenv').config();
-
 var
-    port = process.env.PORT || 3000,
-
     socketIO = require('socket.io'),
     express = require('express'),
     UUID = require('node-uuid'),
-
-    http = require('http'),
-    app = express(),
-    server = http.createServer(app);
+    http = require('http');
 
 // All our pals
 var pals = {};
 
+// IP counter
+var ipCount = {};
+var ipMax = 5;
+var banList = [];
+
 /* Express server set up. */
+
+var port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
 
 // Tell the server to listen for incoming connections
 server.listen(port);
@@ -32,17 +33,12 @@ app.get('/', function(req, res) {
     });
 });
 
-///
-/// This is ridiculous, and should be replaced with Express static files
-///
+// This is ridiculous, and should be replaced with Express static files
 app.get('/*', function(req, res, next) {
-
-    //This is the current file they have requested
+    // This is the current file they have requested
     var file = req.params[0];
-
-    //Send the requesting client the file.
+    // Send the requesting client the file.
     res.sendFile(__dirname + '/' + file);
-
 });
 
 /* Socket.IO server set up. */
@@ -53,50 +49,69 @@ var io = socketIO.listen(server);
 // Socket.io will call this function when a client connects
 io.sockets.on('connection', function(client) {
 
-    // Request client info
-    client.emit("handshake", function(response) {
-        if (!response) return;
+    // Update IP Count
+    var ip = client.handshake.headers["x-forwarded-for"].split(",")[0];
+    if (ipCount[ip])
+      ipCount[ip]++;
+    else
+      ipCount[ip] = 1;
 
-        let clientX = 0, 
-            clientY = 0;
+    // Request client info (if IP count not exceeded)
+    if (ipCount[ip] && (ipCount[ip] < ipMax) && (banList.indexOf(ip) < 0)) {
+      client.emit('handshake', function(response) {
+          if (!response) return;
 
-        // Client has user ID already, so receive info (reconnection)
-        if (response.userId) {
-            client.userId = response.userId;
-            clientX = response.x;
-            clientY = response.y
-        }
-        // New client; set up new user
-        else {
-            // Generate a new UUID and attach it to their socket/connection
-            client.userId = UUID();
+          let clientX = 0, 
+              clientY = 0;
 
-            // Send connection feedback with id and existing pals
-            client.emit('connection_established', {
-                id: client.userId,
-                allPals: pals
-            });
+          // Receive info from client (in case of reconnection)
+          if (response.userId && ) {
+              client.userId = response.userId;
+              clientX = response.x;
+              clientY = response.y
+          }
+          // Set up brand new client
+          else {
+              // Generate a new UUID and attach it to their socket/connection
+              client.userId = UUID();
 
-            // Announce arrival to all connected clients
-            io.emit("arrival", {
-                id: client.userId
-            });
-        }
+              // Send connection feedback with id and existing pals
+              client.emit('connection_established', {
+                  id: client.userId,
+                  allPals: pals
+              });
 
-        // Add client to the pal list
-        pals[client.userId] = {
-            x: clientX,
-            y: clientY
-        };
+              // Announce arrival to all connected clients
+              io.emit("arrival", {
+                  id: client.userId
+              });
+          }
 
-        console.log("✧ Pal " + client.userId + " connected.");
-    });
+          // Add client to the pal list
+          pals[client.userId] = {
+              x: clientX,
+              y: clientY
+          };
+
+          console.log("✧ Pal " + client.userId + "connected from " + ip);
+      });
+    }
+    else {
+      // Send connection feedback with id and existing pals
+      client.emit('connection_established', {
+          id: null,
+          allPals: pals
+      });
+    }
 
     /* Listen for more client events */
 
     // Client motion
-    client.on("motion", function(info) {
-        if (pals[client.userId]) {
+    client.on('motion', function(info) {      
+        if (pals[client.userId] 
+            && (banList.indexOf(ip) < 0)
+            && (typeof info.x == 'number' && typeof info.y == 'number'))  
+        {          
             // Update all clients
             io.emit("motion", {
                 userId: client.userId,
@@ -106,12 +121,18 @@ io.sockets.on('connection', function(client) {
 
             // Update this client in the pal list
             pals[client.userId].x = info.x;
-            pals[client.userId].y = info.x;
+            pals[client.userId].y = info.y;
         }
     });
 
     // Client disconnect
     client.on('disconnect', function() {
+        // Update IP Count
+        if (ipCount[ip])
+          ipCount[ip]--;
+        if (ipCount[ip] == 0)
+          delete ipCount[ip];
+
         // Delete this client from list
         delete pals[client.userId];
         // Announce to all clients
